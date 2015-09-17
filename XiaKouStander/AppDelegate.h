a3cd3173ded7193efc589380f2b54b4eb07108ca
teddy <815819325@qq.com>
2015-09-17 09:38:18 +0800

    Initial Commit

---
+++ XiaKouStander/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaKouStander
+//
+//  Created by teddy on 15/9/17.
+//  Copyright (c) 2015年 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XiaKouStander/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiaKouStander
+//
+//  Created by teddy on 15/9/17.
+//  Copyright (c) 2015年 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
